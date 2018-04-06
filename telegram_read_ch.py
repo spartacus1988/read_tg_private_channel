bc45af0e7c1856692b392413246cc662af8c6de6
mixxxxx <spartacusxxxxxx@gmail.com>
2018-04-07 02:06:24 +0300

    ff

--- telegram_read_ch.py
+++ telegram_read_ch.py
@@ -1,53 +1,18 @@
-# from pytg import Telegram
-
-# tg = Telegram(telegram="~/read_tg_ch/telegram-cli/tg/bin/telegram-cli", pubkey_file="~/read_tg_ch/telegram-cli/tg/tg-server.pub")
-
-
-#receiver = tg.receiver
-# sender = tg.sender
-
-
-
-# sender.send_msg("@spartacusxxxxxx", "Hello World!")
-
-
-
-#tg.msg("@spartacusxxxxxx", "Hello World!")
-
-
-
-
-#/usr/local/src/tg/bin/telegram-cli --json -k /usr/local/src/tg/tg-server.pub -W -d -P 8089 &
-
-
-#~/read_tg_ch/telegram-cli/tg/bin/telegram-cli --json -k ~/read_tg_ch/telegram-cli/tg/tg-server.pub -W -d -P 8089 &
-
-
-
-
-
-
-
-# from pytg.sender import Sender
-
-# sender = Sender("127.0.0.1", 8089)
-   
-
-# res = sender.msg("@spartacusxxxxxx", "Hello!")
-
-
-
+from pytg.sender import Sender
 from pytg.receiver import Receiver
 from pytg.utils import coroutine
 
+import json
 
 @coroutine
 def example_function(receiver):
 	try:
 		while True:
+		
+
 			msg = (yield)
 
-			#print('Full dump: {array}'.format(array=str(msg)))
+			print('Full dump: {array}'.format(array=str(msg)))
 			
 			if msg.event == "message":
 				print(msg)
@@ -59,7 +24,40 @@ def example_function(receiver):
 
 
 if __name__ == '__main__':
-	receiver = Receiver(port=8089)  				# get a Receiver Connector instance
-	receiver.start()  								# start the Connector.
-	receiver.message(example_function(receiver))  	# add "example_function" function as listeners. You can supply arguments here (like receiver).
-	receiver.stop()									# continues here, after exiting while loop in example_function()
+	receiver = Receiver(port=8089)  				
+	sender = Sender(host="127.0.0.1", port=8089) 
+
+	#result = sender.dialog_list()
+	#print(str(result)) 
+
+	result = sender.history('$050000007011c644f7af479d18e7ddac', limit=1, offset=1)
+
+	result = json.dumps(result, sort_keys=True, indent=4, separators=(',', ': '))
+	#print(str(result))
+
+	result = json.loads(result)
+	#print(str(result))
+
+	#print(result[0]['text'])
+
+
+	print(result[0])
+
+	# for item in result:
+	# 	print(item['text'])
+		
+
+
+	#print(str(result['to'])) 
+
+
+
+
+	#receiver.start()  								
+	#receiver.message(example_function(receiver))  	
+	#receiver.stop()									
+
+
+
+
+
