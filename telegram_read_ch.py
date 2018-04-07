e9488ed7dba6e1192b5b60c22a8c13876cb944e2
mixxxxx <spartacusxxxxxx@gmail.com>
2018-04-07 11:47:02 +0300

    it's work

--- telegram_read_ch.py
+++ telegram_read_ch.py
@@ -3,16 +3,17 @@ from pytg.receiver import Receiver
 from pytg.utils import coroutine
 
 import json
+import sys
 
 @coroutine
-def example_function(receiver):
+def listen_function(receiver):
 	try:
 		while True:
 		
 
 			msg = (yield)
 
-			print('Full dump: {array}'.format(array=str(msg)))
+			#print('Full dump: {array}'.format(array=str(msg)))
 			
 			if msg.event == "message":
 				print(msg)
@@ -23,39 +24,28 @@ def example_function(receiver):
 		print("Exiting")
 
 
+def history_function(sender, chat_id, deep_history):
+	result = sender.history(chat_id, limit=1, offset=1)
+	result = json.dumps(result, sort_keys=True, indent=4, separators=(',', ': '))
+	result = json.loads(result)
+	reverse_list = []
+	for item in result[:deep_history]:
+		reverse_list.insert(0, item['text'])
+		
+	for item in reverse_list:
+		print(item)
+
+
+
 if __name__ == '__main__':
 	receiver = Receiver(port=8089)  				
 	sender = Sender(host="127.0.0.1", port=8089) 
 
-	#result = sender.dialog_list()
-	#print(str(result)) 
-
-	result = sender.history('$050000007011c644f7af479d18e7ddac', limit=1, offset=1)
-
-	result = json.dumps(result, sort_keys=True, indent=4, separators=(',', ': '))
-	#print(str(result))
-
-	result = json.loads(result)
-	#print(str(result))
-
-	#print(result[0]['text'])
-
-
-	print(result[0])
-
-	# for item in result:
-	# 	print(item['text'])
-		
-
-
-	#print(str(result['to'])) 
-
-
-
-
-	#receiver.start()  								
-	#receiver.message(example_function(receiver))  	
-	#receiver.stop()									
+	history_function(sender, '$050000007011c644f7af479d18e7ddac', int(sys.argv[1]))
+	
+	receiver.start()  								
+	receiver.message(listen_function(receiver))  	
+	receiver.stop()									
 
 
 
