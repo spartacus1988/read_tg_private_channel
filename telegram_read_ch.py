aae33bbef78754f974a7151cec797461b86b59be
mixxxxx <spartacusxxxxxx@gmail.com>
2018-04-10 01:24:44 +0300

    telethon++

--- telegram_read_ch.py
+++ telegram_read_ch.py
@@ -1,97 +1,3 @@
-								
-# from telethon import TelegramClient
-# from telethon import utils
-# from telethon import events
-# from telethon.tl.functions.messages import SearchRequest
-# from telethon.tl.types import InputMessagesFilterEmpty
-
-# import time
-
-
-
-
-
-# def extract_credentials(pathfile):
-# 	 with open(pathfile, 'r') as f:
-# 			for line in f:
-# 				api_id, api_hash = line.strip().split(':')
-# 				return api_id, api_hash
-
-# api_id, api_hash = extract_credentials('credentials.txt')
-# client = TelegramClient('session_name', api_id, api_hash, update_workers=1, spawn_read_thread=False)
-# client.start()
-
-
-
-# def history_function(chat_name, list_of_queries, min_date, limit):	
-# 	filter = InputMessagesFilterEmpty()
-# 	for query in list_of_queries:	
-# 		result = client(SearchRequest(
-# 			peer=chat_name,      		# On which chat/conversation
-# 			q=query,      				# What to search for
-# 			filter=filter,  			# Filter to use (maybe filter for media)
-# 			min_date=min_date,  		# Unix time
-# 			max_date=None,  			# Maximum date
-# 			offset_id=0,    			# ID of the message to use as offset
-# 			add_offset=0,   			# Additional offset
-# 			limit=limit,       			# How many results
-# 			max_id=0,       			# Maximum message ID
-# 			min_id=0,       			# Minimum message ID
-# 			from_id=None    			# Who must have sent the message (peer)
-# 		))
-# 		#print(result.messages[0].message)
-# 		print(result.messages)
-
-
-# # @client.on(events.NewMessage(chats=('Signal Profits')))
-# # def keywords_handler(event, keywords):
-# # 	for keyword in keywords:
-# # 	    if keyword in event.raw_text:
-# # 	    	print(event.raw_text)
-
-
-
-
-# #@client.on(events.NewMessage(outgoing=True))
-# def keywords_handler():
-# 	print("keywords_is" + 'keywords')
-# 	# for keyword in keywords:
-# 	# 	if keyword in event.raw_text:
-# 	# 		print(event.raw_text)
-		  
-
-# client.add_event_handler(keywords_handler(), event)
-# client.idle()
-
-
-# # if __name__ == '__main__':
-
-# # 	#Set param for searching
-# # 	chat_name = 'Signal Profits'
-# # 	keywords = ['buy', 'target', 'stoploss']
-# # 	now = int(time.time())
-# # 	#print(now)
-# # 	yesterday = now - 86400
-# # 	limit = 100
-
-# # 	history_function(chat_name, keywords, yesterday, limit)
-
-# # 	#keywords_handler(keywords)
-# # 	client.idle()
-
-
-	
-# 	#from telethon.tl.functions.channels import GetFullChannelRequest
-# 	#res = GetFullChannelRequest(chat_name)
-# 	#res = client(GetFullChannelRequest(chat_name))
-# 	#print(str(res))
-
-# 	# from telethon.tl.functions.channels import GetMessagesRequest
-# 	# res = client(GetMessagesRequest(chat_name))
-# 	# print(str(res))
-
-
-
 
 from telethon import TelegramClient
 from telethon import utils
@@ -135,16 +41,20 @@ def history_function(chat_name, list_of_queries, min_date, limit):
 
 
 
-@client.on(events.NewMessage)
-def keywords_handler(event):
-    if '12' in event.raw_text:
-    	print("ss")
+# @client.on(events.NewMessage)
+# def keywords_handler(event):
+# 	if '12' in event.raw_text:
+# 		print("ss")
 
-# @client.on(events.NewMessage(chats=('Signal Profits')))
-# def keywords_handler(event, keywords):
 # 	for keyword in keywords:
-# 	    if keyword in event.raw_text:
-# 	    	print(event.raw_text)
+# 		if keyword in event.raw_text:
+# 			print(event.raw_text)
+
+@client.on(events.NewMessage(chats=('Signal Profits')))
+def keywords_handler(event):
+	for keyword in keywords:
+	    if keyword in event.raw_text:
+	    	print(event.raw_text)
 
 
 
@@ -161,10 +71,6 @@ if __name__ == '__main__':
 
 	history_function(chat_name, keywords, yesterday, limit)
 
-	#keywords_handler(keywords)
-	#client.idle()
 	print('main')
-
-
 
 	client.idle()
